49ec5714e00de6bde66cf5aaf92111ac4fa1c8b6
Aniketk8104 <kushwahaaniket141>
2025-05-27 15:50:16 +0530

    QR

--- src/components/AdminDashboard/AdminDashboard.js
+++ src/components/AdminDashboard/AdminDashboard.js
@@ -5,32 +5,35 @@ import ManageLaptops from "./ManageLaptops";
 import ManageMobileNumbers from "./ManageMobileNumbers";
 import ErrorMessage from "../Shared/ErrorMessage";
 import QRdemo from "./QRdemo";
+import { Container } from "lucide-react";
 
 const AdminDashboard = () => {
   const [currentSection, setCurrentSection] = useState("manageSlideshow");
   const [error, setError] = useState("");
 
   return (
-    <div style={{ display: "flex", height: "100vh", marginTop: "120px" }}>
-      {/* Sidebar Navigation */}
-      <Sidebar
-        currentSection={currentSection}
-        onSectionChange={setCurrentSection}
-      />
+    <section className="mainbody">
+      <div style={{ display: "flex", height: "100vh", marginTop: "120px" }}>
+        {/* Sidebar Navigation */}
+        <Sidebar
+          currentSection={currentSection}
+          onSectionChange={setCurrentSection}
+        />
 
-      {/* Main Content */}
-      <div style={{ flex: 1, padding: "20px", backgroundColor: "#ecf0f1" }}>
-        {error && <ErrorMessage message={error} />}
-        {currentSection === "manageSlideshow" && (
-          <ManageSlideshow setError={setError} />
-        )}
-        {currentSection === "manageLaptops" && (
-          <ManageLaptops setError={setError} />
-        )}
-        {currentSection === "manageMobileNumbers" && <ManageMobileNumbers />}
-        {currentSection === "QRdemo" && <QRdemo />}
+        {/* Main Content */}
+        <div style={{ flex: 1, padding: "20px", backgroundColor: "#ecf0f1" }}>
+          {error && <ErrorMessage message={error} />}
+          {currentSection === "manageSlideshow" && (
+            <ManageSlideshow setError={setError} />
+          )}
+          {currentSection === "manageLaptops" && (
+            <ManageLaptops setError={setError} />
+          )}
+          {currentSection === "manageMobileNumbers" && <ManageMobileNumbers />}
+          {currentSection === "QRdemo" && <QRdemo />}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
